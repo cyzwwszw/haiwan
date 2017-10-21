5a63949db37e483ca8b56e356c85bf581100017e
shylqian <422931278@qq.com>
2017-10-21 13:06:24 +0800

    create the project

---
+++ src/main/java/com/lincomb/haiwan/HaiwanApplication.java
@@ -0,0 +1,12 @@
+package com.lincomb.haiwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaiwanApplication.class, args);
+	}
+}
