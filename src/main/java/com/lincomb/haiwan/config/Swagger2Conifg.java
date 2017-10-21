aead542223c0d8f7373bd51ed99c7547bfe65541
shylqian <422931278@qq.com>
2017-10-21 14:41:08 +0800

    添加swagger

---
+++ src/main/java/com/lincomb/haiwan/config/Swagger2Conifg.java
@@ -0,0 +1,41 @@
+package com.lincomb.haiwan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author: shylqian
+ * @Description:
+ * @Date: created on 下午1:38 17/10/21
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Conifg {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.one"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("spring rest")
+                .termsOfServiceUrl("http://baidu.com")
+                .contact("duke_qian")
+                .version("1.0")
+                .build();
+    }
+}
