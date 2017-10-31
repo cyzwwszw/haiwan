edae26a0321c15403b0f10ef0b491edb36513c6c
yongsheng.he <17621280512@163.com>
2017-10-31 15:07:09 +0800

    修改产品详情

--- src/main/java/com/lincomb/haiwan/service/impl/ProductServiceImpl.java
+++ src/main/java/com/lincomb/haiwan/service/impl/ProductServiceImpl.java
@@ -115,7 +115,8 @@ public class ProductServiceImpl implements ProductService {
             detailsVO.setProductDescription(product.getProductDescription());
             detailsVO.setProductAddress(product.getProductAddress());
             detailsVO.setProductPrice(product.getProductPrice());
-            detailsVO.setProductPic(FastDFSUtil.DOWNLOAD_PATH + product.getProductPic());
+
+            detailsVO.setProductPic(StringUtil.null2String(product.getProductPic()) == "" ? "" : FastDFSUtil.DOWNLOAD_PATH + product.getProductPic());
             List<Map<String, String>> maps = new ArrayList<>();
 
             if (product.getIsHaveYard() == 0) {
